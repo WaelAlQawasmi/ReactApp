f44295c6e5c7c6e1bb22ec49e2aa9eec5ac492b4
waelalq <wael.abuomr@gmail.com>
2022-05-03 01:54:22 +0300

    add prpes

---
+++ src/componants/Body.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Body extends  React.Component {
+    render() {
+      return <h1>A body {this.props.thename}</h1>;
+    }
+  }
+  export default Body
