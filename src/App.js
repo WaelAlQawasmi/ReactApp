00c8d38e8d69bec6544a2f3d090e405d7001b066
waelalq <wael.abuomr@gmail.com>
2022-05-02 04:04:57 +0300

    install app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componants/Header';
+import Footer from './componants/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
