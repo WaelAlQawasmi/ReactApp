49dfe381797c4bfd2551ca4757e5890beb789df9
waelalq <wael.abuomr@gmail.com>
2022-05-05 12:32:10 +0300

    connect with backend- spring boot

--- src/componants/Body.js
+++ src/componants/Body.js
@@ -3,9 +3,31 @@ import React, { Component, useState } from "react";
 class Body extends  React.Component {
     render() {
   
-      return (<div><h1>A body {this.props.thename}</h1>
- 
+      return (
+   <section id="hero" className="hero d-flex align-items-center">
 
-      </div>)    }
+<div className="container">
+  <div className="row">
+    <div className="col-lg-6 d-flex flex-column justify-content-center">
+      {/* <h1 data-aos="fade-up"> {this.props.thename}</h1> */}
+      <h2 >This project to connect front end (reacr js) wih back end spring boot</h2>
+      <div >
+        <div className="text-center text-lg-start">
+          <a href="#about" className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+            <span>Get Started</span>
+            <i className="bi bi-arrow-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <img src="assets/img/hero-img.png" className="img-fluid" alt=""/>
+    </div>
+  </div>
+</div>
+
+</section>
+
+      )    }
   }
   export default Body
